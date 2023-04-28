d16a453f9e486cf7b5a8825b191cc506581c7bae
Sinnwidriger <sinnwidriger@gmai.com>
2023-04-28 02:11:02 +0200

    Inherit MainWindow class from ReativeWindow<ViewModel>

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using ReactiveUI;
+using System.Windows;
 
 namespace Calculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
