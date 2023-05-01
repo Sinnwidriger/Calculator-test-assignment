9ddf08c333b2f9e39d449211bc0d7e05e400524c
Sinnwidriger <sinnwidriger@gmai.com>
2023-05-01 13:34:10 +0200

    Create Calculator View and layout buttons

--- Calculator/Views/TitleBarView.xaml.cs
+++ Calculator/Views/TitleBarView.xaml.cs
@@ -6,9 +6,9 @@ namespace Calculator.Views
     /// <summary>
     /// Interaction logic for TitleBar.xaml
     /// </summary>
-    public partial class TitleBar : ReactiveUserControl<TitleBarViewModel>
+    public partial class TitleBarView : ReactiveUserControl<TitleBarViewModel>
     {
-        public TitleBar()
+        public TitleBarView()
         {
             InitializeComponent();
         }
